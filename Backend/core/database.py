304121b70b9a5a203884ecc7740b9ac8a97943fb
Tihon <miha311008@gmail.com>
2026-06-01 11:08:48 +0300

    feat(core): Добавил функцию для получения сессии и создал сессии

---
+++ Backend/core/database.py
@@ -0,0 +1,16 @@
+from .config import settings
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+async_engine = create_async_engine(
+    url=settings.DATABASE_URL_asyncpg,
+    echo=True
+)
+
+async_session_factory = async_sessionmaker(
+    async_engine,
+    expire_on_commit=False
+)
+
+async def get_db():
+    async with async_session_factory() as session:
+        yield session
