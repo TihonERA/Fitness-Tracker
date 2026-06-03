e3b5612998444bc8f719bec840fb5704fde565c6
Tihon <miha311008@gmail.com>
2026-06-03 18:11:19 +0300

    feat(tests): Добавил тест для метода get_workout

--- Backend/tests/conftest.py
+++ Backend/tests/conftest.py
@@ -1,8 +1,21 @@
 import pytest
-from fastapi.testclient import TestClient
+from httpx import AsyncClient, ASGITransport
 from Backend.main import app
+import sys
+import asyncio
+
+@pytest.fixture(scope="session")
+def event_loop():
+    if sys.platform == "win32":
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    yield loop
+    loop.close()
 
 @pytest.fixture(scope="function")
-def client():
-    with TestClient(app) as c:
+async def client(event_loop):
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url="http://test") as c:
         yield c
--- Backend/tests/test_workout.py
+++ Backend/tests/test_workout.py
@@ -1,6 +1,7 @@
 import pytest
 
-def test_create_workout_with_schedule(client):
+@pytest.mark.asyncio(scope="session")
+async def test_create_workout_with_schedule(client):
     data = {
         "user_id": "00000000-0000-0000-0000-000000000000",
         "name": "Split",
@@ -17,6 +18,14 @@ def test_create_workout_with_schedule(client):
         }]
     }
 
-    result = client.post("/workout/workout_schedule", json=data)
+    result = await client.post("/workout/workout_schedule", json=data)
     assert result.status_code == 201
         
+@pytest.mark.asyncio(scope="session")
+async def test_get_workout(client):
+    response1 = await client.get("/workout/1")
+    assert response1.status_code == 200
+    
+    response_invalid = await client.get("/workout/123")
+    assert response_invalid.status_code == 404
+    assert response_invalid.json()["detail"] == "Not Found"
