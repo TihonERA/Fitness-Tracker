302f07965c0ecced4f129468b3deb6aa07f51305
Tihon <miha311008@gmail.com>
2026-06-03 18:22:19 +0300

    feat(database): Добавил фунцию для получения бд redis

--- Backend/core/database.py
+++ Backend/core/database.py
@@ -1,5 +1,6 @@
 from .config import settings
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from redis import Redis
 
 async_engine = create_async_engine(
     url=settings.DATABASE_URL_asyncpg,
@@ -14,3 +15,6 @@ async_session_factory = async_sessionmaker(
 async def get_db():
     async with async_session_factory() as session:
         yield session
+
+def get_redis():
+    return Redis(settings.REDIS_HOST, settings.REDIS_PORT, decode_responses=True)
