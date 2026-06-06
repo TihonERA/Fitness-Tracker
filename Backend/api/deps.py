4e99fb21477cae5efe8f503206b5d3520c6ed5ea
Tihon <miha311008@gmail.com>
2026-06-06 22:04:37 +0300

    feat(api): Добавил эндпоинт для получения всех тренировок, добавил зависимость для получения Query-параметров этого эндпоинта

--- Backend/api/deps.py
+++ Backend/api/deps.py
@@ -1,8 +1,21 @@
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from ..services.WorkoutService import WorkoutService
 from redis import Redis
+from fastapi import Query
+from ..schemas.workout import WorkoutsFilter
+from ..services.WorkoutService import WorkoutService
 from ..core.database import get_db, get_redis
 
 def get_workout_service(db: AsyncSession = Depends(get_db), redis: Redis = Depends(get_redis)):
     return WorkoutService(db=db, redis=redis)
+
+def get_workouts_filter(
+    skip: int = Query(0, ge=0),
+    limit: int = Query(50, gt=0, le=500),
+    public: bool = Query(False)
+) -> WorkoutsFilter:
+    return WorkoutsFilter(
+        skip=skip,
+        limit=limit,
+        public=public
+    )
--- Backend/api/v1/workout.py
+++ Backend/api/v1/workout.py
@@ -1,22 +1,38 @@
-from fastapi import APIRouter, Depends, status, Path, HTTPException
+from fastapi import APIRouter, Depends, status, Path, HTTPException, Query, Body
 from typing import Annotated
-from ...schemas.workout import WorkoutResponse, WorkoutScheme
+from uuid import UUID
+from ...schemas.workout import WorkoutResponse, WorkoutScheme, WorkoutsFilter
 from ...services.WorkoutService import WorkoutService
 from ...utils.validators import NotFound
-from ..deps import get_workout_service
+from ..deps import get_workout_service, get_workouts_filter
 
 router = APIRouter(
     tags=["Workout Tables Endpoints"]
 )
 
 @router.get(
-    "/workout/{workout_id}",
+    "/workouts",
+    response_model=list[WorkoutResponse],
+    status_code=status.HTTP_200_OK
+)
+async def get_all_workouts(
+    user_id: UUID,
+    filter: Annotated[WorkoutsFilter, Depends(get_workouts_filter)],
+    workout_service: Annotated[WorkoutService, Depends(get_workout_service)]
+):
+    return await workout_service.get_all_workouts(
+        filter=filter,
+        user_id=user_id
+    )
+
+@router.get(
+    "/workouts/{workout_id}",
     response_model=WorkoutResponse,
     status_code=status.HTTP_200_OK      
 )
 async def get_workout(
     workout_id: Annotated[int, Path()],
-    workout_service: WorkoutService = Depends(get_workout_service)
+    workout_service: Annotated[WorkoutService, Depends(get_workout_service)]
     ) -> WorkoutResponse:
     try:
         return await workout_service.get_workout(workout_id)
@@ -24,12 +40,12 @@ async def get_workout(
         raise HTTPException(status_code=e.status_code, detail=e.detail)
 
 @router.post(
-    "/workout/workout_schedule", 
+    "/workouts/workout_schedule", 
     response_model=WorkoutResponse, 
     status_code=status.HTTP_201_CREATED
 )
 async def create_workout_with_schedule(
-        data: WorkoutScheme,
-        workout_service: WorkoutService = Depends(get_workout_service)
+        data: Annotated[WorkoutScheme, Body()],
+        workout_service: Annotated[WorkoutService, Depends(get_workout_service)]
     ) -> WorkoutResponse:
     return await workout_service.create_workout_with_schedule(data)
