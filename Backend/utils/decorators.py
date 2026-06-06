e0fb048f455ff1151c516af868870c59e5297016
Tihon <miha311008@gmail.com>
2026-06-06 22:03:25 +0300

    fix(utils): Зарефакторил decorator чтоб он мог валидировать данные если они отправлены ему в списке

--- Backend/utils/decorators.py
+++ Backend/utils/decorators.py
@@ -10,17 +10,22 @@ def cache(expire: Union[int, timedelta], response_model: Type[BaseModel]):
         @wraps(func)
         async def wrapper(self, *args, **kwargs):
             redis_client = self.redis
+
             args_json = json.dumps({"args": args, "kwargs": kwargs}, sort_keys=True, default=str)
             arguments_hash = hashlib.md5(args_json.encode()).hexdigest()
             key = f"{func.__module__}:{func.__name__}:{arguments_hash}"
+
+            adapter = TypeAdapter(response_model)
+
             cached_data = redis_client.get(key)
             if cached_data:
-                return response_model.model_validate_json(cached_data)
+                return adapter.validate_json(cached_data)
+            
             result = await func(self, *args, **kwargs)
             if result:
-                adapter = TypeAdapter(response_model)
                 result_json = adapter.dump_json(result).decode("utf-8")
                 redis_client.set(key, value=result_json, ex=expire)
+
             return result
         return wrapper
     return decorator
