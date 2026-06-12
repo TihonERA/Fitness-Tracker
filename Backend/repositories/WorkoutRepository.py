e47196c37448c761254da076688ff1c46e7bc10a
TihonERA <miha311008@gmail.com>
2026-06-12 16:40:30 +0300

    feat(repositories): Изменил работу get_all_workouts: возвращает только их id, добавил первую версию метода update_day_exercise, пока что не тестировал

--- Backend/repositories/WorkoutRepository.py
+++ Backend/repositories/WorkoutRepository.py
@@ -1,8 +1,10 @@
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession 
 from ..models.workout import Workout
 from sqlalchemy.orm import selectinload
-from sqlalchemy import select
+from sqlalchemy import select, update, bindparam, and_, CursorResult
 from ..models.trainingday import TrainingDay
+from ..models.dayexercise import  DayExercise
+from typing import Type, Sequence, Any
 from uuid import UUID
 
 class WorkoutRepository:
@@ -10,11 +12,11 @@ class WorkoutRepository:
     def __init__(self, db: AsyncSession):
         self.db = db
 
-    async def create_workout_with_schedule(self, workout: Workout) -> Workout | None:
+    async def create_workout_with_schedule(self, workout: Workout) -> Workout:
         self.add(workout)
         await self.commit()
-
-        return await self.get_workout(workout.workout_id)
+        await self.refresh(workout)
+        return workout
 
     async def get_workout(self, workout_id: int) -> Workout | None:
         stmt = (
@@ -31,23 +33,53 @@ class WorkoutRepository:
     async def get_all_workouts(self, 
         skip: int, 
         limit: int, 
-        user_id: UUID|None=None, 
+        user_id: UUID | None= None, 
         public: bool = False
-    ):
+    ) -> Sequence[int]:
         stmt = (
-            select(Workout)
+            select(Workout.workout_id)
             .where(Workout.public == public)
         )
         if user_id:
             stmt = stmt.where(Workout.user_id == user_id)
 
-        stmt = stmt.offset(skip).limit(limit).options(
-            selectinload(Workout.training_days)
-            .selectinload(TrainingDay.day_exercises)
-        )
+        stmt = stmt.offset(skip).limit(limit)
 
         result = await self.execute(stmt)
         return result.scalars().all()
+    
+    async def update_day_exercise(self,
+        day_exercises: list[dict[str, Any]]
+    ) -> int:
+          data = [
+            {
+                "p_day_id": ex["day_did"],
+                "p_exercise_id": ex["exercise_id"],
+                "p_order": ex["exercise_order"],
+                "p_sets": ex["sets"],
+                "p_reps": ex["reps"]
+            }
+            for ex in day_exercises if ex.get("exercise_id") and ex.get("day_id") 
+          ]
+
+          stmt = (
+            update(DayExercise)
+            .where(
+                and_(
+                    DayExercise.day_id == bindparam("p_day_id"),
+                    DayExercise.exercise_id == bindparam("p_exercise_id")
+                )
+            )
+            .values(
+                exercise_order = bindparam("p_order"),
+                sets = bindparam("p_sets"),
+                reps = bindparam("p_reps")
+            )
+          )
+
+          result = await self.db.execute(stmt, data) # type: ignore
+          return result.rowcount # type: ignore
+
     
     def add(self, instance: object) -> None:
         self.db.add(instance)
@@ -61,4 +93,4 @@ class WorkoutRepository:
     async def execute(self, stmt):
         return await self.db.execute(stmt)
 
-    
+   
