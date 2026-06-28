7ec756ef7fab63217b0445778913f33142d86000
TihonERA <miha311008@gmail.com>
2026-06-28 18:21:20 +0300

    feat(tests): Исправил проблемы что были разные сессии у пайтеста и мейн приложения фастапи и добавил фикстюры для создания даты тренировки и еще я делал это 7 часов и я хочу покончить со всем этим

--- Backend/tests/conftest.py
+++ Backend/tests/conftest.py
@@ -1,32 +1,106 @@
 import pytest
 from httpx import AsyncClient, ASGITransport
-from sqlalchemy.engine import create
 from Backend.main import app
-import sys
-import asyncio
+from sqlalchemy import text
+import uuid
+from ..core.database import async_session_factory
 from ..models.base import Base
+from ..models.user import User
+from ..models.exercise import Exercise
 from ..core.database import async_engine 
+from ..api.deps import get_db
+from sqlalchemy.ext.asyncio import AsyncSession
 
-@pytest.fixture(scope="session")
-def event_loop():
-    if sys.platform == "win32":
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    yield loop
-    loop.close()
-
-@pytest.fixture(scope="function")
-async def client(event_loop):
-    transport = ASGITransport(app=app)
-    async with AsyncClient(transport=transport, base_url="http://test") as c:
-        yield c
-
-@pytest.fixture(autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 async def setup_and_teardown_database():
     async with async_engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
-    
+
+
+    async with async_session_factory() as session:
+        user = User(
+            user_id=uuid.UUID("00000000-0000-0000-0000-000000000000"),
+            login="test_user",
+            email="test@example.com"
+        )
+
+        push_ups = Exercise(
+            name="push_ups",
+            muscle_activation={
+                "pectoralis_major": 0.45,
+                "triceps": 0.35,
+                "anterior_deltoid": 0.20
+            }
+        )
+
+        Dumbbell_Bicep_Curl = Exercise(
+            name="Dumbbell_Bicep_Curl",
+            muscle_activation={
+                "biceps_brachii": 0.80,
+                "brachialis": 0.20
+            }
+        )
+
+        Crunches = Exercise(
+            name="Crunches",
+            muscle_activation={
+                "rectus_abdominis": 0.80,
+                "obliques": 0.20
+            }
+        )
+        
+        session.add_all([user, push_ups, Dumbbell_Bicep_Curl, Crunches])
+        
+        await session.commit()
+
     yield
+
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+@pytest.fixture(scope="function")
+async def client(db_session: AsyncSession):
+    
+    app.dependency_overrides[get_db] = lambda: db_session
+    
+    transport = ASGITransport(app=app) # type: ignore
+    async with AsyncClient(transport=transport, base_url="http://test") as c:
+        yield c
+        
+    app.dependency_overrides.clear()
+
+@pytest.fixture(scope="function", autouse=True)
+async def clean_tables():
+    yield
+
+    async with async_engine.begin() as conn:
+        await conn.execute(text("TRUNCATE TABLE workout, trainingday, dayexercise CASCADE;"))
+
+@pytest.fixture
+def make_workout_data():
+    def _make_data(name="Split", description="TestDescription...", day_name="Day1", day_order=1):
+        return {
+            "user_id": "00000000-0000-0000-0000-000000000000",
+            "name": name,
+            "description": description,
+            "training_days": [{
+                "name": day_name,
+                "day_order": day_order,
+                "day_exercises": [{
+                    "exercise_id": 1,
+                    "exercise_order": 1,
+                    "sets": 4,
+                    "reps": 15
+                }]
+            }]
+        }
+    return _make_data
+
+@pytest.fixture(scope="function")
+async def db_session():
+    """Фикстура изолированной сессии базы данных для конкретного теста."""
+    async with async_session_factory() as session:
+        await session.begin_nested()
+        yield session
+        await session.rollback()
