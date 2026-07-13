057dbc8d06b09202d1f592bb5b7a62a2803f4940
TihonERA <miha311008@gmail.com>
2026-07-13 09:44:25 +0300

    feat: Добавил директорию tasks, добавил две задачи целери: get_muscles_balance, get_muscles_distribution_list

---
+++ Backend/tasks/__init__.py
@@ -0,0 +1,3 @@
+from .muscle_rates import cel_app, get_muscle_contribution_list, get_muscles_balance
+
+__all__ = ['cel_app', 'get_muscle_contribution_list', 'get_muscles_balance']
---
+++ Backend/tasks/muscle_rates.py
@@ -0,0 +1,60 @@
+# Backend/tasks/muscle_rates.py
+import asyncio
+import logging
+from celery import Celery
+from sqlalchemy import NullPool
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from ..services.WorkoutService import WorkoutService
+from ..core.database import get_redis
+from ..core import celeryconfig
+from ..core.config import settings
+
+logger = logging.getLogger(__name__)
+
+cel_app = Celery("workout_rate")
+cel_app.config_from_object(celeryconfig)
+
+def get_isolated_sessionmaker():
+    engine = create_async_engine(
+        settings.DATABASE_URL_asyncpg, 
+        poolclass=NullPool
+    )
+    return async_sessionmaker(bind=engine, expire_on_commit=False)
+
+@cel_app.task
+def get_muscles_balance(workout_id: int):
+    
+    try:
+        async def _run():
+            sessionmaker = get_isolated_sessionmaker()
+            async with sessionmaker() as session:
+                redis = get_redis()
+                service = WorkoutService(db=session, redis=redis)
+                
+                result = await service.get_muscles_balance(workout_id=workout_id)
+                return result
+        
+        return asyncio.run(_run())
+        
+    except Exception as e:
+        logger.error(f"=== ОШИБКА В ЗАДАЧЕ: {e} ===")
+        logger.error(f"=== ТИП ОШИБКИ: {type(e)} ===")
+        logger.error(f"=== TRACEBACK ===", exc_info=True)
+        raise
+
+@cel_app.task
+def get_muscle_contribution_list(workout_id: int):
+    try:
+        async def _run():
+            sessionmaker = get_isolated_sessionmaker()
+            async with sessionmaker() as session:
+                redis = get_redis()
+                service = WorkoutService(db=session, redis=redis)
+                return await service.get_muscles_distribution_list(workout_id=workout_id)
+        
+        return asyncio.run(_run())
+    except Exception as e:
+        logger.error(f"=== ОШИБКА В ЗАДАЧЕ: {e} ===")
+        logger.error(f"=== ТИП ОШИБКИ: {type(e)} ===")
+        logger.error(f"=== TRACEBACK ===", exc_info=True)
+        raise
