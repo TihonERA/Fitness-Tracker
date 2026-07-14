8a8f538c23fc1feba374900c413a51fde23b9b6d
TihonERA <miha311008@gmail.com>
2026-07-14 20:00:13 +0300

    feat: Добавил репозиторий для таблицы TrainingDay

---
+++ Backend/repositories/TrainingDayRepository.py
@@ -0,0 +1,32 @@
+from typing import Any
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from Backend.models.trainingday import TrainingDay
+
+from .SqlAlchemyAbstractRepository import SQLAlchemyAbstractRepository
+
+class TrainingDayRepository(SQLAlchemyAbstractRepository[TrainingDay]):
+    def __init__(self, session: AsyncSession):
+        self.session = session
+        super().__init__(session, TrainingDay)
+
+    async def update_training_day(
+        self,
+        day_id: int,
+        data: dict[str, Any]
+    ) -> int:
+        return await self.update_by_column(
+            column=self.model.day_id,
+            identificator=day_id,
+            data=data
+        )
+
+    async def delete_training_day(
+        self,
+        day_id: int
+    ) -> int:
+        return await self.delete_by_column(
+            column=self.model.day_id,
+            identificator=day_id
+        )
