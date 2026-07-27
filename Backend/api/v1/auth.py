63bc7a500ad394d1bd5ed853ba46bd832d7aaf2a
TihonERA <miha311008@gmail.com>
2026-07-27 20:21:16 +0300

    feat(api): Добавил эндпоинты: /auth/users, /auth/login, /auth/refresh

---
+++ Backend/api/v1/auth.py
@@ -0,0 +1,102 @@
+from typing import Annotated
+
+from fastapi import Body, Cookie, APIRouter, HTTPException, Response, status
+
+from Backend.api.deps import AuthServiceDepends
+from Backend.schemas.auth import UserAuthorize
+from Backend.schemas.user import UserCreate
+from Backend.utils.validators import InvalidCredentials
+
+router = APIRouter(
+   tags=["Authorization Endpoints"],
+   prefix="/auth"
+)
+
+def set_access_and_refresh_cookie(
+    response: Response,
+    access_token: str,
+    refresh_token: str
+):
+    response.set_cookie(
+        key="access_token",
+        value=access_token,
+        secure=True,
+        httponly=True,
+        samesite='lax'
+    )
+    response.set_cookie(
+        key="refresh_token",
+        value=refresh_token,
+        secure=True,
+        httponly=True,
+        samesite='lax'
+    )
+
+@router.post(
+    "/users",
+    status_code=status.HTTP_200_OK
+)
+async def register(
+    data: Annotated[UserCreate, Body()],
+    auth_service: AuthServiceDepends,
+    response: Response
+):
+    try:
+        tokens = await auth_service.register(data=data)
+        
+        set_access_and_refresh_cookie(
+            response=response,
+            access_token=tokens.access_token,
+            refresh_token=tokens.refresh_token
+        )
+        return status.HTTP_200_OK
+    except InvalidCredentials as e:
+        raise HTTPException(
+            status_code=e.status_code,
+            detail=e.detail
+        )
+
+@router.post(
+    "/login",
+    status_code=status.HTTP_200_OK
+)
+async def login(
+    data: Annotated[UserAuthorize, Body()],
+    auth_service: AuthServiceDepends,
+    response: Response
+):
+    try:
+        tokens = await auth_service.login(data=data)
+        
+        set_access_and_refresh_cookie(
+            response=response,
+            access_token=tokens.access_token,
+            refresh_token=tokens.refresh_token
+        )
+        return status.HTTP_200_OK
+    except InvalidCredentials as e:
+        raise HTTPException(status_code=e.status_code, detail=e.detail)
+
+@router.post(
+    "/refresh",
+    status_code=status.HTTP_200_OK
+)
+async def refresh(
+    auth_service: AuthServiceDepends,
+    response: Response,
+    refresh_token: Annotated[str | None, Cookie()] = None,
+):
+    try:
+        tokens = await auth_service.refresh(token=refresh_token)
+
+        set_access_and_refresh_cookie(
+            response=response,
+            access_token=tokens.access_token,
+            refresh_token=tokens.refresh_token
+        )
+        return status.HTTP_200_OK
+    except InvalidCredentials as e:
+        raise HTTPException(
+            status_code=e.status_code,
+            detail=e.detail
+        )
