c7a8a9595ec5a6877364477846bb5c7a4092446a
TihonERA <miha311008@gmail.com>
2026-07-29 22:19:22 +0300

    feat(Backend/tests/test_user.py): Добавил тесты для эндпоинтов: GET: /users/me, /users/{user_id}; PATCH: /users/me; DELETE: /users/me

---
+++ Backend/tests/test_user.py
@@ -0,0 +1,43 @@
+from httpx import AsyncClient
+import pytest
+
+@pytest.mark.asyncio(loop_scope="session")
+class TestUser:
+
+    async def test_get_me(self, client: AsyncClient, make_user_data):
+        await make_user_data()
+        response = await client.get("/users/me")
+
+        print(response.json())
+        assert response.status_code == 200
+        assert len(response.json()) > 0
+
+    async def test_get_user(self, client: AsyncClient, make_user_data):
+        await make_user_data()
+        user = await client.get("/users/me")
+        user_id = user.json().get("user_id")
+        response = await client.get(f"/users/{user_id}")
+
+        assert response.status_code == 200
+        assert len(response.json()) > 0
+
+    async def test_update_user(self, client: AsyncClient, make_user_data):
+        await make_user_data()
+        
+        update_data = {
+            "login": "newupdatedlogin"
+        }
+        response = await client.patch("/users/me", json=update_data)
+
+        assert response.status_code == 200
+        assert response.json().get("login") == update_data.get("login")
+
+    async def test_delete_user(self, client: AsyncClient, make_user_data):
+        await make_user_data()
+
+        response = await client.delete("/users/me")
+
+        deleted_user = await client.get("/users/me")
+
+        assert response.status_code == 200
+        assert deleted_user.status_code == 404
