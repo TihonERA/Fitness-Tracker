365bde9910740918104ed83ab2b0fff7db65ea1e
TihonERA <miha311008@gmail.com>
2026-07-30 23:50:27 +0300

    feat(Backend/tests/test_auth.py): Убрал создание пользователья, теперь пользователь создается в conftest

--- Backend/tests/test_user.py
+++ Backend/tests/test_user.py
@@ -4,16 +4,16 @@ import pytest
 @pytest.mark.asyncio(loop_scope="session")
 class TestUser:
 
-    async def test_get_me(self, client: AsyncClient, make_user_data):
-        await make_user_data()
+    async def test_get_me(self, client: AsyncClient, authorize_user):
+        await authorize_user()
         response = await client.get("/users/me")
 
         print(response.json())
         assert response.status_code == 200
         assert len(response.json()) > 0
 
-    async def test_get_user(self, client: AsyncClient, make_user_data):
-        await make_user_data()
+    async def test_get_user(self, client: AsyncClient, authorize_user):
+        await authorize_user()
         user = await client.get("/users/me")
         user_id = user.json().get("user_id")
         response = await client.get(f"/users/{user_id}")
@@ -21,8 +21,8 @@ class TestUser:
         assert response.status_code == 200
         assert len(response.json()) > 0
 
-    async def test_update_user(self, client: AsyncClient, make_user_data):
-        await make_user_data()
+    async def test_update_user(self, client: AsyncClient, authorize_user):
+        await authorize_user()
         
         update_data = {
             "login": "newupdatedlogin"
@@ -32,8 +32,8 @@ class TestUser:
         assert response.status_code == 200
         assert response.json().get("login") == update_data.get("login")
 
-    async def test_delete_user(self, client: AsyncClient, make_user_data):
-        await make_user_data()
+    async def test_delete_user(self, client: AsyncClient, authorize_user):
+        await authorize_user()
 
         response = await client.delete("/users/me")
 
