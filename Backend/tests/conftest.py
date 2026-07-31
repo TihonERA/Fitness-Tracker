ce4220e2d4f5576c8b15f54f99fa41bbaff7c89e
TihonERA <miha311008@gmail.com>
2026-07-31 21:00:41 +0300

    feat(Backend/tests/conftest): Вынес в отдельную фикстюру создание тренировки

--- Backend/tests/conftest.py
+++ Backend/tests/conftest.py
@@ -1,8 +1,10 @@
 import asyncio
 import pytest
 from httpx import AsyncClient, ASGITransport
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 from Backend.main import app
-from sqlalchemy import text
+from sqlalchemy import select, text
 from sqlalchemy.dialects.postgresql import insert
 import uuid
 from Backend.tasks.muscle_rates import cel_app
@@ -44,7 +46,7 @@ async def db_session():
         await session.rollback()
 
 @pytest.fixture(scope="session", autouse=True)
-async def setup_and_teardown_database(workout_data):
+async def setup_and_teardown_database():
     alembic_cfg = Config(str(alembic_ini_path))
     await asyncio.to_thread(command.upgrade, alembic_cfg, "head")
 
@@ -57,35 +59,7 @@ async def setup_and_teardown_database(workout_data):
             hash_password="$argon2id$v=19$m=65536,t=3,p=4$b04HUqHrntSERdQIO+Nz0A$hmNV+lnF9Y6t46tCnXfBJEIxH4MEZZuE69h8RCjBmhA"
         )
 
-        workout = Workout(
-            user_id=user.user_id,
-            name=workout_data.get("name"),
-            description=workout_data.get("description")
-        )
-
-        session.add_all([workout, user])
-        await session.flush()
-
-        for tr_day in workout_data.get("training_days", []):
-            training_day = TrainingDay(
-                name=tr_day.get("name"),
-                day_order=tr_day.get("day_order"),
-                workout_id=workout.workout_id
-            )
-            session.add(training_day)
-            await session.flush()
-
-            for day_ex in tr_day.get("day_exercises", []):
-                day_exercise = DayExercise(
-                    day_id=training_day.day_id,
-                    exercise_id=day_ex.get("exercise_id"),
-                    workout_id=workout.workout_id,
-                    exercise_order=day_ex.get("exercise_order"),
-                    sets=day_ex.get("sets"),
-                    reps=day_ex.get("reps")
-                )
-                session.add(day_exercise)
-
+        session.add(user)
         await session.commit()
 
     yield
@@ -98,52 +72,59 @@ async def clear_redis():
 
     await redis.aclose()
 
-@pytest.fixture(scope="session")
-def workout_data():
-    data = {
-        "user_id": "00000000-0000-0000-0000-000000000000",
-        "name": "testworkout",
-        "description": "testdescriptiontestdescriptiontestdescription",
-        "training_days": [
-            {
-                "name": "Тренировка спины и бицепса",
-                "day_order": 1,
-                "day_exercises": [
-                    {"exercise_id": 9, "exercise_order": 1, "sets": 4, "reps": 15},  
-                    {"exercise_id": 10, "exercise_order": 2, "sets": 4, "reps": 15},
-                    {"exercise_id": 11, "exercise_order": 3, "sets": 4, "reps": 15}, 
-                    {"exercise_id": 18, "exercise_order": 4, "sets": 4, "reps": 15}, 
-                    {"exercise_id": 29, "exercise_order": 5, "sets": 4, "reps": 15}, 
-                    {"exercise_id": 43, "exercise_order": 6, "sets": 4, "reps": 15}  
-                ]
-            },
-            {
-                "name": "Тренировка груди, плеч и трицепса",
-                "day_order": 2,
-                "day_exercises": [
-                    {"exercise_id": 3, "exercise_order": 1, "sets": 4, "reps": 15}, 
-                    {"exercise_id": 5, "exercise_order": 2, "sets": 4, "reps": 15}, 
-                    {"exercise_id": 21, "exercise_order": 3, "sets": 4, "reps": 15},
-                    {"exercise_id": 22, "exercise_order": 4, "sets": 4, "reps": 15},
-                    {"exercise_id": 37, "exercise_order": 5, "sets": 4, "reps": 15},  
-                    {"exercise_id": 45, "exercise_order": 6, "sets": 4, "reps": 15}  
-                ]
-            },
-            {
-                "name": "Тренировка ног и пресса",
-                "day_order": 3,
-                "day_exercises": [
-                    {"exercise_id": 47, "exercise_order": 1, "sets": 4, "reps": 15}, 
-                    {"exercise_id": 17, "exercise_order": 2, "sets": 4, "reps": 15}, 
-                    {"exercise_id": 52, "exercise_order": 3, "sets": 4, "reps": 15}, 
-                    {"exercise_id": 59, "exercise_order": 4, "sets": 4, "reps": 15}, 
-                    {"exercise_id": 42, "exercise_order": 5, "sets": 4, "reps": 15}, 
-                    {"exercise_id": 40, "exercise_order": 6, "sets": 4, "reps": 15}  
-                ]
-            }
-            ]
-        } 
-    return data
+@pytest.fixture
+async def make_workout(db_session: AsyncSession):
+    day_1 = TrainingDay(name="Тренировка спины и бицепса", day_order=1)
+    day_2 = TrainingDay(name="Тренировка груди, плеч и трицепса", day_order=2)
+    day_3 = TrainingDay(name="Тренировка ног и пресса", day_order=3)
+
+    ex_1_1 = DayExercise(exercise_id=9, exercise_order=1, sets=4, reps=15, training_day=day_1)
+    ex_1_2 = DayExercise(exercise_id=10, exercise_order=2, sets=4, reps=15, training_day=day_1)
+    ex_1_3 = DayExercise(exercise_id=11, exercise_order=3, sets=4, reps=15, training_day=day_1)
+    ex_1_4 = DayExercise(exercise_id=18, exercise_order=4, sets=4, reps=15, training_day=day_1)
+    ex_1_5 = DayExercise(exercise_id=29, exercise_order=5, sets=4, reps=15, training_day=day_1)
+    ex_1_6 = DayExercise(exercise_id=43, exercise_order=6, sets=4, reps=15, training_day=day_1)
+
+    ex_2_1 = DayExercise(exercise_id=3, exercise_order=1, sets=4, reps=15, training_day=day_2)
+    ex_2_2 = DayExercise(exercise_id=5, exercise_order=2, sets=4, reps=15, training_day=day_2)
+    ex_2_3 = DayExercise(exercise_id=21, exercise_order=3, sets=4, reps=15, training_day=day_2)
+    ex_2_4 = DayExercise(exercise_id=22, exercise_order=4, sets=4, reps=15, training_day=day_2)
+    ex_2_5 = DayExercise(exercise_id=37, exercise_order=5, sets=4, reps=15, training_day=day_2)
+    ex_2_6 = DayExercise(exercise_id=45, exercise_order=6, sets=4, reps=15, training_day=day_2)
+
+    ex_3_1 = DayExercise(exercise_id=47, exercise_order=1, sets=4, reps=15, training_day=day_3)
+    ex_3_2 = DayExercise(exercise_id=17, exercise_order=2, sets=4, reps=15, training_day=day_3)
+    ex_3_3 = DayExercise(exercise_id=52, exercise_order=3, sets=4, reps=15, training_day=day_3)
+    ex_3_4 = DayExercise(exercise_id=59, exercise_order=4, sets=4, reps=15, training_day=day_3)
+    ex_3_5 = DayExercise(exercise_id=42, exercise_order=5, sets=4, reps=15, training_day=day_3)
+    ex_3_6 = DayExercise(exercise_id=40, exercise_order=6, sets=4, reps=15, training_day=day_3)
+
+    workout = Workout(
+        user_id=uuid.UUID("00000000-0000-0000-0000-000000000000"),
+        name="testworkout",
+        description="testdescription",
+        training_days=[day_1, day_2, day_3],
+        day_exercises=[
+            ex_1_1, ex_1_2, ex_1_3, ex_1_4, ex_1_5, ex_1_6,
+            ex_2_1, ex_2_2, ex_2_3, ex_2_4, ex_2_5, ex_2_6,
+            ex_3_1, ex_3_2, ex_3_3, ex_3_4, ex_3_5, ex_3_6
+        ]
+    ) 
+
+    db_session.add(workout)
+    await db_session.flush()
+
+    stmt = (
+        select(Workout)
+        .where(Workout.workout_id == workout.workout_id)
+        .options(
+            selectinload(Workout.training_days)
+            .selectinload(TrainingDay.day_exercises)
+        )
+    )
+    workout = await db_session.execute(stmt)
+    await db_session.commit()
+    return workout.scalar()
 
 @pytest.fixture
 async def authorize_user(client, db_session):
@@ -153,6 +134,7 @@ async def authorize_user(client, db_session):
             "password": "registration_data_password"
         } 
         user_data = await client.post(f"/auth/login", json=login_data)
+        print(user_data.json())
         await db_session.commit() 
         return user_data
     return _make_data
