332392ca9499aca2016555872c0d352e2949febc
TihonERA <miha311008@gmail.com>
2026-07-31 20:53:56 +0300

    feat(Backend/repositories/TrainingDayRepository): Добавил методы для получения модели: обновления и проверки доступа

--- Backend/repositories/TrainingDayRepository.py
+++ Backend/repositories/TrainingDayRepository.py
@@ -1,10 +1,12 @@
 from typing import Any
+from uuid import UUID
 
-from sqlalchemy import select
+from sqlalchemy import join, select
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import selectinload
+from sqlalchemy.orm import InstrumentedAttribute, selectinload
 
 from Backend.models.trainingday import TrainingDay
+from Backend.models.workout import Workout
 
 from .SqlAlchemyAbstractRepository import SQLAlchemyAbstractRepository
 
@@ -28,16 +30,32 @@ class TrainingDayRepository(SQLAlchemyAbstractRepository[TrainingDay]):
         result = await self.session.execute(stmt)
         return result.scalar_one_or_none()
 
-    async def update_training_day(
+    async def get_training_day_for_update(
         self,
-        day_id: int,
-        data: dict[str, Any]
-    ):
-        return await self.update_by_column(
-            column=self.model.day_id,
-            identificator=day_id,
-            data=data
+        day_id: int
+    ) -> TrainingDay | None:
+        return await self.get_instance_for_update(
+            column=TrainingDay.day_id,
+            identificator=day_id
         )
+
+    async def get_training_day_and_check_access(
+        self,
+        user_id: UUID,
+        workout_id: int,
+        day_id: int,
+    ) -> TrainingDay | None:
+        stmt = (
+            select(TrainingDay)
+            .join(Workout, TrainingDay.workout_id == Workout.workout_id)
+            .where(
+                Workout.user_id == user_id,
+                Workout.workout_id == workout_id,
+                TrainingDay.day_id == day_id
+            )
+        )
+        result = await self.execute(stmt)
+        return result.scalar_one_or_none()
 
     async def delete_training_day(
         self,
