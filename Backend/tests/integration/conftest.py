cc77db364a37231ef4add7bc1df61dddeaa98403
TihonERA <miha311008@gmail.com>
2026-08-14 13:40:02 +0300

    refactor(Backend/tests/integration/conftest.py): Фикстюра прежде удаляющая и создавающая таблицы, теперь просто очищает их от данных

--- Backend/tests/integration/conftest.py
+++ Backend/tests/integration/conftest.py
@@ -45,25 +45,19 @@ async def setup_migrations():
 
 @pytest.fixture(scope="function", autouse=True)
 async def setup_and_teardown_database():
-
-    target_tables = cast(
-        list[Table],
-        [
-            User.__table__, 
-            Workout.__table__, 
-            TrainingDay.__table__, 
-            DayExercise.__table__, 
-            TrainingDayHistory.__table__, 
-            ExerciseHistory.__table__
-        ]
-    )
     async with async_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all, tables=target_tables)
-
-    yield
-
+        await conn.execute(text(
+            'TRUNCATE TABLE "user", workout, trainingday, dayexercise, '
+            'trainingdayhistory, exercisehistory RESTART IDENTITY CASCADE;'
+        ))
+        
+    yield  
+    
     async with async_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all, tables=target_tables)
+        await conn.execute(text(
+            'TRUNCATE TABLE "user", workout, trainingday, dayexercise, '
+            'trainingdayhistory, exercisehistory RESTART IDENTITY CASCADE;'
+        ))
 
 @pytest.fixture(scope="function", autouse=True)
 async def clear_redis():
@@ -100,15 +94,16 @@ async def workout(db_session: AsyncSession, user):
     ex_3_5 = DayExercise(exercise_id=42, exercise_order=5, training_day=day_3)
     ex_3_6 = DayExercise(exercise_id=40, exercise_order=6, training_day=day_3)
 
+    day_1.day_exercises = [ex_1_1, ex_1_2, ex_1_3, ex_1_4, ex_1_5, ex_1_6]
+    day_2.day_exercises = [ex_2_1, ex_2_2, ex_2_3, ex_2_4, ex_2_5, ex_2_6]
+    day_3.day_exercises = [ex_3_1, ex_3_2, ex_3_3, ex_3_4, ex_3_5, ex_3_6]
+
     workout = Workout(
         user_id=user.id,
         name="testworkout",
         description="testdescription",
-        training_days=[day_1, day_2, day_3],
-        day_exercises=[
-            ex_1_1, ex_1_2, ex_1_3, ex_1_4, ex_1_5, ex_1_6,
-            ex_2_1, ex_2_2, ex_2_3, ex_2_4, ex_2_5, ex_2_6,
-            ex_3_1, ex_3_2, ex_3_3, ex_3_4, ex_3_5, ex_3_6
+        training_days=[
+            day_1, day_2,day_3        
         ]
     ) 
 
