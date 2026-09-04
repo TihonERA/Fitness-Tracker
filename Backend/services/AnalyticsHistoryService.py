82d9a1f99021544f3c6cff8cbfa085d442728a62
TihonERA <miha311008@gmail.com>
2026-09-04 18:25:46 +0300

    refactor(Backend/service/AnalyticsHistoryService.py): Элементы добавляются в массив в конце итерации, начал разработку второй версии метода создания

--- Backend/services/AnalyticsHistoryService.py
+++ Backend/services/AnalyticsHistoryService.py
@@ -40,6 +40,42 @@ class AnalyticsHistoryService:
             return None
         return difference
 
+    async def create_history_v2(
+        self, user_id: UUID, data: CreateHistory
+    ) -> HistoryResponse:
+        async with self.uow as uow:
+            new_create_data = TrainingDayHistoryCreate(
+                day_name=data.day_name, day_id=data.day_id
+            )
+
+            last_training = await uow.trainingdayhistory.get_last_history(user_id)
+            new_training = await uow.trainingdayhistory.create_instance(new_create_data)
+
+            if last_training is None:
+                sobiraem_new_training_prost()  # type: ignore
+                return HistoryResponse(new_training=new_training)
+
+            last_training_exercise_id_set = {
+                ex.exercise_id for ex in last_training.exercises_history
+            }
+
+            last_index = 0
+            new_index = 0
+
+            while new_index < len(data.exercises):
+                ex_create_dto_data = ExerciseHistoryCreateDTO(
+                    user_id=user_id,
+                    training_day_history_id=new_training.id,
+                    **data.exercises[new_index].model_dump(exclude_unset=True),
+                )
+                new = await uow.exercisehistory.create_exercise_history(
+                    ex_create_dto_data
+                )
+
+                if new.exercise_id not in last_training_exercise_id_set:
+                    new_index += 1
+                    continue
+
     async def create_history(
         self, user_id: UUID, data: CreateHistory
     ) -> HistoryResponse:
@@ -73,11 +109,10 @@ class AnalyticsHistoryService:
                 )
                 new = await uow.exercisehistory.create_exercise_history(exercise_data)
 
-                differences.append(ExerciseDifference(exercise_id=new.exercise_id))
-                current_difference = differences[new_tr_index]
+                difference = ExerciseDifference(exercise_id=new.exercise_id)
 
                 if last.exercise_id != new.exercise_id:
-                    current_difference.exercise_id = None
+                    difference.exercise_id = None
                     new_tr_index += 1
                     continue
 
@@ -85,10 +120,10 @@ class AnalyticsHistoryService:
                 new_sets_length = len(new.sets_history)
 
                 for i in range(max(last_sets_length, new_sets_length)):
-                    current_difference.sets_differences.append(SetsDifference())
-
                     if i >= last_sets_length or i >= new_sets_length:
                         continue
+
+                    current_set = SetsDifference()
 
                     old_current_set = last.sets_history[i]
                     new_current_set = new.sets_history[i]
@@ -96,21 +131,22 @@ class AnalyticsHistoryService:
                     if new_current_set.reps is None or old_current_set.reps is None:
                         continue
 
-                    current_difference.sets_differences[i].reps = (
-                        self.calc_diff_return_none_if_zero(
-                            first=new_current_set.reps,
-                            second=old_current_set.reps,
-                        )
+                    current_set.reps = self.calc_diff_return_none_if_zero(
+                        first=new_current_set.reps,
+                        second=old_current_set.reps,
                     )
                     if new_current_set.weight is None or old_current_set.weight is None:
                         continue
 
-                    current_difference.sets_differences[i].weight = (
-                        self.calc_diff_return_none_if_zero(
-                            first=new_current_set.weight,
-                            second=old_current_set.weight,
-                        )
+                    current_set.weight = self.calc_diff_return_none_if_zero(
+                        first=new_current_set.weight,
+                        second=old_current_set.weight,
                     )
+
+                    difference.sets_differences.append(current_set)
+
+                differences.append(difference)
+
                 new_tr_index += 1
                 last_tr_index += 1
 
