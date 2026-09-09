26755a3a749dab7015b567543eebdafa51c42876
TihonERA <miha311008@gmail.com>
2026-09-09 20:15:03 +0300

    feat(Backend/services/MuscleRatesService.py): Сделал декомпозицию, теперь главная функция отвечает за транзакцию и составление итогового словаря, _calculate_muscle_conf за получение данных из бд, _apply_muscle_coefs_to_base_coefs за подсчет коэффицентов

--- Backend/services/MuscleRatesService.py
+++ Backend/services/MuscleRatesService.py
@@ -1,5 +1,6 @@
 from collections import Counter
 import dataclasses
+from typing import Sequence
 from uuid import UUID
 
 from sqlalchemy import values
@@ -25,23 +26,35 @@ class MuscleRatesService(BaseService):
         muscles_list = await self.uow.musclerepository.get_all_muscles()
         return Counter(dict.fromkeys(muscles_list, 0))
 
+    def _apply_muscle_coefs_to_base_coefs(
+        self, base_coefs: Counter, new_coefs: Sequence[dict[str, float]]
+    ) -> Counter:
+        return sum(
+            (Counter(muscle_data) for muscle_data in new_coefs), start=base_coefs
+        )
+
+    async def _calculate_muscle_coef(self, workout_id) -> Counter:
+        base_muscles_coef = await self._get_dict_muscle_list()
+
+        trained_muscles_coef = (
+            await self.uow.musclerepository.get_all_trained_muscles_from_workout(
+                workout_id
+            )
+        )
+
+        return self._apply_muscle_coefs_to_base_coefs(
+            base_coefs=base_muscles_coef, new_coefs=trained_muscles_coef
+        )
+
     async def get_muscle_distribution_list(
         self, user_id: UUID, workout_id: int
     ) -> dict:
         async with self.uow:
-            all_muscles_with_coef = await self._get_dict_muscle_list()
-
-            all_trained_muscles_in_workout = (
-                await self.uow.musclerepository.get_all_trained_muscles_from_workout(
-                    workout_id
-                )
-            )
-            for muscle_data in all_trained_muscles_in_workout:
-                all_muscles_with_coef.update(muscle_data)
+            all_muscles_with_coef = await self._calculate_muscle_coef(workout_id)
 
             result = {
-                name: self.get_muscle_status(score)
-                for name, score in all_muscles_with_coef.items()
+                muscle: self.get_muscle_status(score)
+                for muscle, score in all_muscles_with_coef.items()
             }
 
             return result
